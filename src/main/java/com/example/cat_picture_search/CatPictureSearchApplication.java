1cb21f29da011df621c4fdd8aed8da88f2d2f198
youngji <youngji.jang804@gmail.com>
2023-04-06 13:34:05 +0900

    init

---
+++ src/main/java/com/example/cat_picture_search/CatPictureSearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.cat_picture_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatPictureSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatPictureSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cat_picture_search/CatPictureSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cat_picture_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatPictureSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
