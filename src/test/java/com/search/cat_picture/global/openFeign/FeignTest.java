0c662602d4e4b1eb2db149df546a84ed776defdf
youngji <youngji.jang804@gmail.com>
2023-04-12 19:38:15 +0900

    db 추가

--- src/test/java/com/search/cat_picture/global/openFeign/FeignTest.java
+++ src/test/java/com/search/cat_picture/global/openFeign/FeignTest.java
@@ -9,7 +9,9 @@ import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.http.HttpMessageConvertersAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 @ImportAutoConfiguration({
 	OpenFeignConfig.class,
--- src/test/java/com/search/cat_picture/service/TheCatPictureServiceTest.java
+++ src/test/java/com/search/cat_picture/service/TheCatPictureServiceTest.java
@@ -1,12 +1,13 @@
 package com.search.cat_picture.service;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.search.cat_picture.dto.CatPictureResponse;
@@ -14,8 +15,7 @@ import com.search.cat_picture.dto.CatPictureResponses;
 import com.search.cat_picture.global.exception.BadRequestException;
 import com.search.cat_picture.repository.CatPictureRepository;
 
-import lombok.RequiredArgsConstructor;
-
+@ActiveProfiles("test")
 @SpringBootTest
 @Transactional
 class TheCatPictureServiceTest {
@@ -28,7 +28,7 @@ class TheCatPictureServiceTest {
 
 	@Test
 	@DisplayName("랜덤 고양이 사진 조회")
-	void getRandomPictures(){
+	void getRandomPictures() {
 		CatPictureResponses responses = catPictureService.getRandomPictures(10);
 
 		assertThat(responses.catPictures().size()).isEqualTo(10);
@@ -36,7 +36,7 @@ class TheCatPictureServiceTest {
 
 	@Test
 	@DisplayName("품종별 고양이 사진 조회")
-	void getPicturesByBreedName(){
+	void getPicturesByBreedName() {
 		CatPictureResponses responses = catPictureService.getPicturesByBreedName("Abyssinian");
 
 		assertThat(responses.catPictures().size()).isEqualTo(1);
@@ -44,7 +44,7 @@ class TheCatPictureServiceTest {
 
 	@Test
 	@DisplayName("존재하지 않는 품종 고양이 사진 조회")
-	void getPicturesByBreedName_notExist(){
+	void getPicturesByBreedName_notExist() {
 		CatPictureResponses responses = catPictureService.getPicturesByBreedName("영조");
 
 		assertThat(responses.catPictures().size()).isEqualTo(0);
@@ -52,7 +52,7 @@ class TheCatPictureServiceTest {
 
 	@Test
 	@DisplayName("사진 id로 고양이 사진 조회_성공")
-	void getPictureById_success(){
+	void getPictureById_success() {
 		catPictureRepository.deleteById("ajs");
 		assertThat(catPictureRepository.findById("ajs").isEmpty()).isTrue();
 		CatPictureResponse response = catPictureService.getPictureById("ajs");
@@ -63,9 +63,9 @@ class TheCatPictureServiceTest {
 
 	@Test
 	@DisplayName("사진 id로 고양이 사진 조회_실패")
-	void getPictureById_theCatApi_fail(){
+	void getPictureById_theCatApi_fail() {
 		assertThat(catPictureRepository.findById("영지").isEmpty()).isTrue();
-		assertThrows(BadRequestException.class, () ->{
+		assertThrows(BadRequestException.class, () -> {
 			catPictureService.getPictureById("영지");
 		});
 	}
